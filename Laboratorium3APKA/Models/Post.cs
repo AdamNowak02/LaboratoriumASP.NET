b6c21dc0595dcc07766e727ac1b93e638d74b09b
AdamNowak02 <adam.nowak@microsoft.wsei.edu.pl>
2023-11-06 19:51:38 +0100

    dodanie LAB4

---
+++ Laboratorium3APKA/Models/Post.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Laboratorium_3___Post_App.Models
+{
+    public class Post
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Musisz wpisać zawartość")]
+        [StringLength(maximumLength: 200, ErrorMessage = "Za dużo znaków!")]
+        [Display(Name = "Aparat")]
+        public string Content {  get; set; }
+        [Display(Name = "Autor")]
+        [Required(ErrorMessage = "Musisz wpisać Autora!")]
+        [StringLength(maximumLength: 50, ErrorMessage = "Za dużo znaków!")]
+        public string Author { get; set; }
+        [Display(Name = "Data i godzina")]
+        [Required(ErrorMessage = "Musisz uzupełnić datę!")]
+        public DateTime Date { get; set; }
+        [Display(Name = "Opis")]
+        [Required(ErrorMessage = "Musisz wpisać opis!")]
+        [StringLength(maximumLength: 100, ErrorMessage = "Za dużo znaków!")]
+        public string Tags { get; set; }
+        [Display(Name = "Opis")]
+        [Required(ErrorMessage = "Musisz wpisać dane!")]
+        [StringLength(maximumLength: 200, ErrorMessage = "Za dużo znaków!")]
+        public string Comment { get; set; }
+        [Display(Name = "Rozdzielczość")]
+        public string Format { get; set; }
+        [Display(Name = "Format")]
+
+        public DateTime Created { get; set; }
+
+    }
+}
